308e9ba0660c4896defdda3420255734a7cd7352
maxim <zanoga.maxim@gmail.com>
2022-06-27 20:38:01 +0100

    update k8 deploy

--- k8s/k8s_deploy.py
+++ k8s/k8s_deploy.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-import os
-import sys
-import random
-import string
-import subprocess
-import requests
-import re
+import base64
 import json
-from pathlib import Path
+import os
+import re
+import sys
+
+import requests
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -20,16 +19,52 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 def touch(path):
     with open(path, 'a'):
         os.utime(path, None)
 
+
 def download_file(url, path):
-	r = requests.get(url)
-	with open(path, 'w') as f:
-		f.write(r.text)
+    r = requests.get(url)
+    with open(path, 'w') as f:
+        f.write(r.text)
+
+
+def replace_configs(path):
+    with open(path, "rt") as file:
+        deployment = file.read()
+
+    with open(path, "wt") as file:
+        deployment = deployment.replace("<app_name>", app_name)
+        deployment = deployment.replace("<hub_url>", hub_url)
+        deployment = deployment.replace("<app_version>", app_version)
+        deployment = deployment.replace("<app_port>", app_port)
+        deployment = deployment.replace("<app_healthcheck>", app_healthcheck)
+        deployment = deployment.replace("<app_domain>", app_domain)
+        file.write(deployment)
+
+
+def update_version(path, new_version):
+    deployment_file = os.path.join(path, "deployment.yaml")
+    with open(deployment_file, "rt") as file:
+        deployment = file.read()
+
+    with open(deployment_file, "wt") as file:
+        deployment = re.sub("(\d+.\d+.\d+)", new_version, deployment)
+        file.write(deployment)
+
+
+def update_secret_hash(secret_file, credentials_hash):
+    with open(secret_file, "rt") as file:
+        secret = file.read()
+
+    with open(secret_file, "wt") as file:
+        secret = secret.replace("<base64_hash>", credentials_hash)
+        file.write(secret)
 
 
 def show_help():
@@ -40,39 +75,62 @@ def show_help():
     print(f'{bcolors.WARNING}- secret{bcolors.ENDC} - config secret to connect docker hub')
     print('')
 
+
 if len(sys.argv) > 1:
-	command = sys.argv[1]
-	if command == 'deploy':
-		app_name = input("APP Name: ")
-		app_version = input("APP Version: ")
-		
-		app_domain = input("APP Domain: ")
-		app_port = input("APP Port: ")
-		app_healthcheck = input("APP Healthcheck (default / ): ")
+    command = sys.argv[1]
+    if command == 'deploy':
+        app_name = input("APP Name: ")
+        app_version = input("APP Version: ")
 
-		hub_url = input("HUB Url: ")
-		env_variables = input("ENV variables --> dict {key:value}: ")
-		
-		if env_variables:
-			env_dict = json.loads(env_variables)
+        app_domain = input("APP Domain: ")
+        app_port = input("APP Port: ")
+        app_healthcheck = input("APP Healthcheck (default / ): ")
 
-		os.mkdir(app_name)
-		os.chdir(app_name)
+        hub_url = input("HUB Url: ")
+        env_variables = input("ENV variables --> dict {key:value}: ")
 
-		download_file("https://raw.githubusercontent.com/ZanMax/homelab/main/k8s/templates/deployment.yaml", "deployment.yaml")
-		download_file("https://raw.githubusercontent.com/ZanMax/homelab/main/k8s/templates/ingress.yaml", "ingress.yaml")
-		download_file("https://raw.githubusercontent.com/ZanMax/homelab/main/k8s/templates/service.yaml", "service.yaml")
+        if env_variables:
+            env_dict = json.loads(env_variables)
 
-		with open("deployment.yaml", "rt") as file:
-			deployment = file.read()
-	
-		with open("deployment.yaml", "wt") as file:
-			deployment = deployment.replace("<app_name>", app_name)
-			file.write(deployment)
+        os.mkdir(app_name)
+        os.chdir(app_name)
 
-	if command == 'redeploy':
-		print('redeploy')
-	if command == 'secret':
-		print('secret')
+        download_file("https://raw.githubusercontent.com/ZanMax/homelab/main/k8s/templates/deployment.yaml",
+                      "deployment.yaml")
+        download_file("https://raw.githubusercontent.com/ZanMax/homelab/main/k8s/templates/ingress.yaml",
+                      "ingress.yaml")
+        download_file("https://raw.githubusercontent.com/ZanMax/homelab/main/k8s/templates/service.yaml",
+                      "service.yaml")
+
+        replace_configs("deployment.yaml")
+        replace_configs("ingress.yaml")
+        replace_configs("service.yaml")
+
+    if command == 'redeploy':
+        app_path = input("APP path: ")
+        mew_app_version = input("New APP Version: ")
+        update_version(app_path, mew_app_version)
+
+    if command == 'secret':
+        user = input("User: ")
+        password = input("Password: ")
+        hub = input("Hub url: ")
+        registry = input("Registry url: ")
+        auth_hash = base64.b64encode('{}:{}'.format(user, password).encode('utf-8'))
+        credentials = {
+            "auths": {
+                hub: {
+                    "auth": auth_hash.decode('utf-8')
+                },
+                registry: {
+                    "auth": auth_hash.decode('utf-8')
+                }
+            }
+        }
+
+        credentials_json = json.dumps(credentials, indent=4)
+        credentials_hash = base64.b64encode(credentials_json.encode('utf-8'))
+        download_file("https://raw.githubusercontent.com/ZanMax/homelab/main/k8s/templates/secret.yaml", "secret.yaml")
+        update_secret_hash("secret.yaml", credentials_hash.decode('utf-8'))
 else:
-	show_help()
+    show_help()
